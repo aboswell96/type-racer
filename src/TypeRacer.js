c0adb672d33b884e65879d720d8bbaeb904004a7
Alex Boswell <aboswell778@gmail.com>
2021-08-02 17:47:21 -0700

    Add components for current word being typed

--- src/TypeRacer.js
+++ src/TypeRacer.js
@@ -13,7 +13,19 @@ const TypeRacer = () => {
         const newChar = e.nativeEvent.data;
 
         if(newChar != " ") {
+
+            if(charCount == 0 && wordCount!==0) {
+                console.log("ddddd");
+                //SetIncompleteWords(incompleteWords.slice(splitWords[wordCount].length));
+            }
+
+            SetCurrentWordIncompleteChars(splitWords[wordCount].slice(charCount + 1));
             SetCurrentWordInput(e.target.value);
+            SetCurrentWordCompletedChars(currentWordCompletedChars.concat(newChar));
+            SetCharCount(charCount + 1);
+
+            console.log(e);
+
             return;
         }
 
@@ -21,12 +33,13 @@ const TypeRacer = () => {
         //console.log(e);
         if(currentWordInput == (splitWords[wordCount])) {
             //Word is correct
-            console.log("dasdsa");
             SetCompletedWords(completedWords.concat(splitWords[wordCount] + " "));
             SetWordCount(wordCount + 1);
             SetCurrentWordInput("");
-            SetIncompleteWords(incompleteWords.slice(splitWords[wordCount].length + 1)) //To-do: When does wordCount get updated?
-            
+            SetIncompleteWords(incompleteWords.slice(splitWords[wordCount + 1].length + 1)) //To-do: When does wordCount get updated?
+            SetCharCount(0);
+            SetCurrentWordCompletedChars("");
+            SetCurrentWordIncompleteChars(splitWords[wordCount+1]);
         }
         else {
             //
@@ -37,15 +50,18 @@ const TypeRacer = () => {
 
     const [currentWordInput, SetCurrentWordInput] = useState("");
     const [completedWords, SetCompletedWords] = useState("");
-    const [currentWordCompleted, SetCurrentWordCompleted] = useState("");
-    const [currentWordIncomplete, SetCurrentWordIncomplete] = useState("");
+    const [currentWordCompletedChars, SetCurrentWordCompletedChars] = useState("");
+    const [currentWordIncompleteChars, SetCurrentWordIncompleteChars] = useState(splitWords[0]);
     const [wordCount, SetWordCount] = useState(0);
-    const [incompleteWords, SetIncompleteWords] = useState(prompt);
+    const [incompleteWords, SetIncompleteWords] = useState(prompt.slice(splitWords[0].length));
+    const [charCount, SetCharCount] = useState(0);
 
     return(
     <div>
         <div class="test-prompt">
             <p class="correct-words">{completedWords}</p>
+            <p class="current-word-correct">{currentWordCompletedChars}</p>
+            <p class="current-word-incomplete">{currentWordIncompleteChars}</p>
             <p class="incomplete-words">{incompleteWords}</p>
         </div>
         <input 
